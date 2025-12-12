43154719dc858f3945b310c1181e55ef811e0406
saviovnn <savioviannasilva@hotmail.com>
2025-12-11 23:14:48 -0300

    feat: adicionando componente de Loading

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,9 @@
 <template>
-  <div>
-    <button @click="navigateTo('/dashboard')" class="bg-blue-500 text-white p-2 rounded-md">Dashboard</button>
-    <h1>Home FlamaAI</h1>
-    <p>{{ authStore.isAuthenticated }}</p>
+  <div class="flex items-center justify-center h-screen">
+    <Loading />
   </div>
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router'
-import { useAuthStore } from '@/stores/auth'
-
-const router = useRouter()
-const authStore = useAuthStore()
-function navigateTo(path) {
-  router.push(path)
-}
+import Loading from '@/components/Loading.vue'
 </script>
